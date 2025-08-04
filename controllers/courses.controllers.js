9566a0668c242b6ddec30f82ddead995713cff4a
MahMoh22 <mahmoud.m.abbass010@gmail.com>
2025-08-04 11:45:05 +0300

    remove all comments

--- controllers/courses.controllers.js
+++ controllers/courses.controllers.js
@@ -15,17 +15,9 @@ asyncWrapper(
             message: 'Courses fetched successfully',
             data: {courses},
         });
-        // } catch (error) {
-            //     res.status(500).json({
-                //         status: statusHelper.ERROR, 
-                //         message: error.message,
-                //         data: null, 
-                //     });
-                // }
             }
         ) ;
 const createCourse = asyncWrapper( async (req, res) => {
-    //try {
         const course = new Course(req.body);
         await course.save();
         
@@ -34,90 +26,44 @@ const createCourse = asyncWrapper( async (req, res) => {
             message: 'Course created successfully',
             data: {course},
         });
-    // } catch (error) {
-    //     res.status(500).json({ 
-    //         status: statusHelper.ERROR,
-    //         message: error.message,
-    //         data: null,
-    //      });
-    // }
+    
 });
 const updateCourse = asyncWrapper( async (req, res, next) => {
-    //try {
         const course = await Course.findByIdAndUpdate(req.params.id, req.body);
         if (!course) {
             const error = ERRORHelper.create('Course not found', 404,statusHelper.FAIL);
             return next(error);
-            // res.status(404).json({ 
-            //     status: statusHelper.FAIL,
-            //     message: 'Course not found' ,
-            //     data: null,
-            // });
         }
         return res.json({ 
             status: statusHelper.SUCCESS,
             message: 'Course updated successfully',
             data: {course},
          });
-    // } catch (error) {
-    //     return res.status(500).json({ 
-    //         status: statusHelper.ERROR,
-    //         message: error.message ,
-    //         data: null,
-    //     });
-    // }
 });
 const deleteCourse = asyncWrapper( async (req, res, next) => {
-    //try{
         const course = await Course.findByIdAndDelete(req.params.id);
         if (!course) {
             const error = ERRORHelper.create('Course not found', 404,statusHelper.FAIL);
             return next(error);
-            // return res.status(404).json({ 
-            //     status: statusHelper.FAIL,
-            //     message: 'Course not found' ,
-            //     data: null,
-            // });
         }
         return res.json({ 
             status: statusHelper.SUCCESS,
             message: 'Course deleted successfully' ,
             data: null,
         });
-    // } catch (error) {
-    //     return res.status(500).json({ 
-    //         status: statusHelper.ERROR,
-    //         message: error.message ,
-    //         data: null,
-    //     });
-
-    // }
 });
 const getCourse = asyncWrapper( async (req, res, next) => {
-    //try {
         const id = req.params.id;
         const course = await Course.findById(req.params.id);
         if (!course) {
             const error = ERRORHelper.create('Course not found', 404,statusHelper.FAIL);
             return next(error);
-            // return res.status(404).json({ 
-            //     status: statusHelper.FAIL,
-            //     message: 'Course not found' ,
-            //     data: null,
-            // });
         }
         return res.json({
             status: statusHelper.SUCCESS,
             message: 'Course fetched successfully',
             data: {course},
         });
-    // } catch (error) {
-    //    return res.status(500).json({ 
-    //     status: statusHelper.ERROR,
-    //     message: error.message ,
-    //     data: null,
-    // });
-    // }
 });
 
 module.exports = {
